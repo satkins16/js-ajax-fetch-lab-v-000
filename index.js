c6c468ea2fe5a31383e7c6534d4e2ac5aa26e595
Scott Atkins <satkins16@gmail.com>
2017-12-02 23:36:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const token = 'YOUR_TOKEN_HERE';
+ 
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
+
 function getIssues() {
 }
 
