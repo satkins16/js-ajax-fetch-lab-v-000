29c6b47d4c118fbf12fbf2a6a6045704118f5736
Scott Atkins <satkins16@gmail.com>
2017-12-03 00:02:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,63 @@
-const token = '0c9b24e4e577ebd0030d51534f64d2feac663f93';
+const userName = ''
+const baseApi = 'https://api.github.com/'
+const fork = `${userName}/javascript-fetch-lab`
 
+//Issue and Repo objects and templates
 
-
-function getIssues() {
+function Issue(attributes){
+  this.title = attributes.title;
+  this.body = attributes.body;
+  this.url = attributes.url;
 }
 
-function showIssues(json) {
+function Repo(attributes){
+  this.url = attributes.url;
 }
+
+Issue.prototype.template = function(){
+   var template = `<li>Title: <a href="${this.url}">${this.title} </a><span> | Body: ${this.body}</span></li>`
+   return template;
+};
+
+Repo.prototype.template = function(){
+  var template = `<h3>Forked Successfully!</h3><a href="${this.url}"> ${this.url}</a>`
+  return template;
+};
+
+//Create an issue through the Github API
 
 function createIssue() {
+  const issueTitle = document.getElementById('title').value
+  const issueBody = document.getElementById('body').value
+  const postData = { title: issueTitle, body: issueBody }
+  fetch(`${baseApi}repos/${fork}/issues`, {
+    method: 'post',
+    headers: {
+      'Authorization': `token ${getToken()}`
+    },
+    body: JSON.stringify(postData)
+  }).then(resp => getIssues())
 }
 
-function showResults(json) {
+//Fetch all issues through the Github API and display / append to the DOM
+
+function getIssues(data) {
+  fetch(`${baseApi}repos/${fork}/issues`).
+    then(resp => {
+      resp.json().then( data => {
+        for (let i = 0; i < data.length; i++){
+          displayIssue(new Issue(data[i]));
+        }
+      } )
+    })
 }
+
+function displayIssue(issue) {
+  $('#issues').append(issue.template())
+}
+
+//Fetch and show repo info
+
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
@@ -26,9 +71,13 @@ function forkRepo() {
     let repo = new Repo(resp);
     showForkedRepo(repo);
   })
+}
+
+function showForkedRepo(repo) {
+  $('#results').append(repo.template())
+}
+
 
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
-  return '7e50f20c2c4344948ac2153a06f68f1a607c8bad'
+  return ''
 }
